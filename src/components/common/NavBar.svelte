c011e8e272f16a08a44ae011f05933eeee43729f
Scynes <dustin.greyfield@gmail.com>
2023-04-21 16:05:24 -0400

    wip/complete: added sticky nav bar based on scroll direction

--- src/components/common/NavBar.svelte
+++ src/components/common/NavBar.svelte
@@ -1,4 +1,21 @@
-<nav class="width-full flex justify-center">
+<script>
+    
+    let showNav = true;
+
+    let previousYOffset = 0;
+
+    const handleScroll = () => {
+
+        showNav = window.pageYOffset < previousYOffset;
+
+        previousYOffset = window.pageYOffset;
+    }
+
+</script>
+
+<svelte:window on:scroll={handleScroll} />
+
+<nav class="width-full flex justify-center" class:show={showNav}>
     <a href="/" class="nav-item">Home</a>
     <a href="/" class="nav-item">About</a>
     <a href="/" class="nav-item">Login</a>
