d575b80f92560f00ed0706fae6f06610a25cd40f
Scynes <dustin.greyfield@gmail.com>
2023-04-21 15:46:51 -0400

    added main page section layout

--- src/components/common/NavBar.svelte
+++ src/components/common/NavBar.svelte
@@ -1,3 +1,6 @@
 <nav>
-    
+    <a href="/">Home</a>
+    <a href="/">About</a>
+    <a href="/">Login</a>
+    <a href="/">Signup</a>
 </nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,15 @@
     /** @type {import('./$types').LayoutData} */
 
     import '@public/global.css';
+
+    // @ts-ignore
+    import NavBar from '@components/common/NavBar.svelte';
     
 </script>
 
 <div class="page-container">
     <header>
-
+        <NavBar />
     </header>
     <main>
         <slot />
