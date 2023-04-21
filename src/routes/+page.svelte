60730035c2d19238ca77eee1f095825eae5a0ef6
mohamed ahmed <mohamedahmed754@hotmail.com>
2023-04-21 16:27:15 -0400

    added a nav bar to the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,22 @@
-<h1 class="text-5xl">Welcome to SvelteKit</h1>
-<p class="text-3xl">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<a class="text-2xl underline"href="/about">Go to the about page</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Genext</title>
+</head>
+<main class="bg-white px-10 md:px-20 lg:px-40">
+<section class="min-h-screen">
+    <nav class="py-10 mb-12 flex justify-between bg-red-600">
+        <h1 class="text-4xl">
+            Genext!
+        </h1>
+    <ul class="flex items-center justify-evenly">
+        <li class=" text-2xl underline"><a href="/about">About</a></li>
+        <li class="text-2xl underline">Log in</li>
+        <li class="text-2xl underline">Sign up</li>
+    </ul>
+    </nav>
+</section>
+</main>
+</html>
