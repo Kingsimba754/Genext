2f1478bf4362cbebdf59f890b6b52162baa3ed8e
Scynes <dustin.greyfield@gmail.com>
2023-04-21 15:22:08 -0400

    added page container layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    /** @type {import('./$types').LayoutData} */
+    export let data;
+</script>
+
+<div class="page-container">
+    <header>
+
+    </header>
+    <main>
+        <slot />
+    </main>
+    <footer>
+        
+    </footer>
+</div>
